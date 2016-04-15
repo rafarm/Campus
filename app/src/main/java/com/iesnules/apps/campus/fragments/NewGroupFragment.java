2bf25a0ebde61c9bd4bf6d414fe1492225d40a3d
robertoroigpedro <rroigpedro@gmail.com>
2016-04-15 11:50:20 +0200

    WIP

--- app/src/main/java/com/iesnules/apps/campus/fragments/NewGroupFragment.java
+++ app/src/main/java/com/iesnules/apps/campus/fragments/NewGroupFragment.java
@@ -1,4 +1,4 @@
-package com.iesnules.apps.campus;
+package com.iesnules.apps.campus.fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.iesnules.apps.campus.R;
 
 
 /**
