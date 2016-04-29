dac7f2d474279c09b6ed383089592bc49778b0cb
IvanSQRS <ivanovenian@icloud.com>
2016-04-29 12:42:41 +0200

    .setHomeAsUpIndicator(...);

--- app/src/main/java/com/iesnules/apps/campus/ProfileActivity.java
+++ app/src/main/java/com/iesnules/apps/campus/ProfileActivity.java
@@ -48,6 +48,7 @@ public class ProfileActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
 
         mProfile = MainActivity.getUserProfile();
 
