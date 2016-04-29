084519dd79ae03c5ba745151e7e5d356619585bf
alumne <gloriisnavarro@gmail.com>
2016-04-29 12:37:27 +0200

    habilitar main activity

--- app/src/main/java/com/iesnules/apps/campus/MainActivity.java
+++ app/src/main/java/com/iesnules/apps/campus/MainActivity.java
@@ -2,6 +2,7 @@ package com.iesnules.apps.campus;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
@@ -30,6 +31,8 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -360,6 +363,19 @@ public class MainActivity extends AppCompatActivity implements
         );
         AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
         */
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.iesnules.apps.campus/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
     }
 
     /*
@@ -558,9 +574,28 @@ public class MainActivity extends AppCompatActivity implements
         progressBar.setVisibility(View.GONE);
     }
 
+    private void enableUI(boolean enable) {
+        mBottomBar.setEnabled(enable);
+        mDrawerNavigationView.setEnabled(enable);
+        mFAB.setEnabled(enable);
+    }
+
     @Override
     public void onStop() {
         super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.iesnules.apps.campus/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
         /*
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -661,6 +696,11 @@ public class MainActivity extends AppCompatActivity implements
         }
 
         @Override
+        protected void onPreExecute() {
+            enableUI(false);
+        }
+
+        @Override
         protected UserProfile doInBackground(GoogleSignInAccount... params) {
             UserRecord record = null;
             GoogleSignInAccount account = params[0];
@@ -688,6 +728,7 @@ public class MainActivity extends AppCompatActivity implements
 
         @Override
         protected void onPostExecute(UserProfile profile) {
+            enableUI(true);
             hideProgressIndicator();
             if (profile != null) { // User profile exists in backend store
                 mUserProfile = profile;
